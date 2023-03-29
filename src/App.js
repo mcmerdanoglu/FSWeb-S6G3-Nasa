e36ca300485c15bd654571c4561e4ae30d9f97bc
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-03-29 23:29:14 +0300

    Tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
+import BiComponent from "./components/BiComponent";
 
 function App() {
+  const [apodData, setApodData] = useState();
+  const [datePicker, setDatePicker] = useState(
+    new Date("2022-08-21").toISOString().slice(0, 10)
+  );
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod",
+        /*"https://api.nasa.gov/planetary/apod?api_key=3wkI5lZxyiVojEhKgnaeG2kJwgzf00RAFI97pnsz"*/ {
+          params: {
+            api_key: "5aTPYSEBtG9uzEFh8PzWtDHaiFSN2W2hWRQHXkgS",
+            date: datePicker,
+          },
+        }
+      )
+      .then(function(response) {
+        console.log(response);
+        setApodData(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+      .finally(function() {});
+    console.log("Sayfa Render oldu!");
+  }, [datePicker]);
+
   return (
     <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <BiComponent
+        data={apodData}
+        dateChange={setDatePicker}
+        currentDate={datePicker}
+      />
     </div>
   );
 }
---
+++ src/components/BiComponent.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function BiComponent(props) {
+  const { data, dateChange, currentDate } = props;
+  function dateChangeHandler(e) {
+    console.log(e.target.value);
+    dateChange(e.target.value);
+  }
+  if (!data) return <h3>Yükleniyor...</h3>;
+  return (
+    <div>
+      <p>{data.date}</p>
+      <label htmlFor="apodDate">apodDate:</label>
+      <input
+        onChange={(e) => dateChangeHandler(e)}
+        type="date"
+        value={currentDate}
+        name="apodDate"
+      ></input>
+      <p>{data.explanation}</p>
+      <p>{data.hdurl}</p>
+      <p>{data.media_type}</p>
+      {data.media_type === "image" && (
+        <img src={data.hdurl} alt={data.title} width="1000" height="600" />
+      )}
+      {data.media_type === "video" && (
+        <iframe width="800" height="600" src={data.url}></iframe>
+      )}
+      <p>{data.service_version}</p>
+      <p>{data.title}</p>
+      <p>{data.url}</p>
+    </div>
+  );
+}
